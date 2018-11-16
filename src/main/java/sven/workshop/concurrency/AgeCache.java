da335f82179148a490cdf7813c9676e898221739
svenakela <sven.wesley@cygni.se>
2018-11-16 13:01:10 +0100

    Exercise 4

---
+++ src/main/java/sven/workshop/concurrency/AgeCache.java
@@ -0,0 +1,47 @@
+package sven.workshop.concurrency;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public final class AgeCache {
+
+  private final Map<String, Data> cacheMap = new HashMap<>();
+
+  Optional<Integer> getAge(final String key) {
+    return Optional.ofNullable(cacheMap.get(key)).map(Data::getAge);
+  }
+
+  void setAge(final String key, final Integer age) {
+    cacheMap.put(key, new Data(age));
+  }
+  
+  Optional<Instant> getCreationTime(String key) {
+    return Optional.ofNullable(cacheMap.get(key)).map(Data::getCreationTime); 
+  }
+
+  void reset(final String key) {
+    cacheMap.remove(key);
+  }
+
+  private final class Data {
+
+    private final Instant creationTime = Instant.now();
+    private final Integer age;
+
+    Data(final Integer age) {
+      this.age = age;
+    }
+
+    public Instant getCreationTime() {
+      return creationTime;
+    }
+
+    public Integer getAge() {
+      return age;
+    }
+
+  }
+
+}
---
+++ src/test/java/sven/workshop/concurrency/AgeCacheTest.java
@@ -0,0 +1,5 @@
+package sven.workshop.concurrency;
+
+public class AgeCacheTest {
+
+}
