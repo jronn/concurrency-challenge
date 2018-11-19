68660dbce9d91c6507e0c1d31b63dd8d363987b7
svenakela <sven.wesley@cygni.se>
2018-11-19 14:07:34 +0100

    Tests

--- src/test/java/sven/workshop/concurrency/AgeCacheTest.java
+++ src/test/java/sven/workshop/concurrency/AgeCacheTest.java
@@ -1,5 +1,22 @@
 package sven.workshop.concurrency;
 
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class AgeCacheTest {
 
+  @Test
+  public void breakit() {
+    
+    var cache = new AgeCache();
+    
+    IntStream.range(0, 1000)
+        .parallel()
+        .forEach(i -> cache.setAge(String.valueOf(i), i));
+    
+    Assertions.assertEquals(1000, cache.size());
+    
+  }
+  
 }
